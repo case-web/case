a98027d6eb8145fd3b47254ef77ddb66a1c93f08
Анна Михеева <miheeva.aa@yandex.ru>
2017-03-19 04:04:09 +0300

    Верстка десктопа. Индекс.

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Cat on 19.03.2017.
+ */
+console.log('poshel nahyi');
+console.log('poshel nahyi');
---
+++ templates/header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CASE WEB TECHNOLOGY</title>
+    <script src="../assets/js/jQuery320.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded fixed-top">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="/"><img src="../assets/images/logo.png" alt=""></a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Услуги <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Портфолио</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Новости</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Контакты</a>
+                </li>
+            </ul>
+            <ul class="list-inline my-2">
+                <li><a class="header_phone" href="#">+7 (812) 123-45-67</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
